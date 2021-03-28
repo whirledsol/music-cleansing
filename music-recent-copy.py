555e8e6a98dd9395c6886b980141a0864b11132c
whirledsol <astro491@gmail.com>
2021-03-27 22:07:48 -0400

    added program to consolidate recent files and copy, used for usb storage

--- music-recent-copy.py
+++ music-recent-copy.py
@@ -1,5 +1,4 @@
-import sys,os,re
-import audio_metadata
+import os
 import argparse
 import shutil
 from datetime import datetime,timedelta
@@ -9,18 +8,24 @@ def main():
     the driver
     '''
     args = parse()
-    files = get_files(args.dir,args.threshold)
-    copy_files(files,args.output_root)
+    files = get_files(args)
+    if confirm(files):
+        copy_files(files,args)
 
   
 def parse():
     '''
     sets the args needed to run and returns them
     '''
+
+    #defaults
+    timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
+    output_root_default = f"./out/recent_{timestamp}/"
+
     parser = argparse.ArgumentParser(description='Creates copies of recently changed files or directories and places them in a local directory with the same folder structure')
     parser.add_argument('-d','--directory',dest='dir',type=str, required=True, help='the directory to find files in')
     parser.add_argument('-t','--timespan','--days', dest='timespan', required=True, help='number of days prior to the current date to search for')
-
+    parser.add_argument('-o','--output', dest='output_root',default=output_root_default, help=f"output path (optional, defaults to {output_root_default})")
     args = parser.parse_args()
 
     #standardize directory
@@ -28,18 +33,17 @@ def parse():
 
     #calculate the min date to search for
     args.threshold = datetime.today() - timedelta(days=int(args.timespan))
-    timestamp = datetime.now().strftime("%Y%m/%dT%H%M%S")
     
-    #output path, always the same
-    args.output_root = f"./output/recent_{timestamp}/"
-
     return args
 
     
-def get_files(directory,threshold):
+def get_files(args):
     '''
     gets the array of files to partition
     '''
+    directory = args.dir
+    threshold = args.threshold
+
     print(f'walking {directory}')
     found = []
     for root,_,files in os.walk(directory):
@@ -65,10 +69,43 @@ def is_music(f):
     return f.split('.')[-1] in music_exts
 
 
-def copy_files(sources, output_root):
+def copy_files(sources, args):
     '''
     copies files from directory to output
     '''
-    print(sources)
+    directory = args.dir
+    output_root = args.output_root
+
+    os.makedirs(os.path.abspath(output_root),exist_ok=True)
+
+    for file in sources:
+        relpath = get_path_relroot(file,directory)
+        newpath = os.path.abspath(os.path.join(output_root,relpath))
+        os.makedirs(os.path.dirname(newpath),exist_ok=True)
+        shutil.copy(file,newpath)
+    
+def confirm(files):
+    '''
+    displays info and asks for confirm
+    '''
+    if len(files) == 0:
+        print('No files found. Exiting.')
+        exit()
+
+    totalsize = sum([os.path.getsize(f) for f in files])/1.0e6
+    print("Found {0} file(s) totalling {1:.2f}MB".format(len(files),totalsize))
+
+    print('Proceed with copying? (Y/n)')
+    choice = input()
+    return choice.upper() == 'Y'
+    
+
+def get_path_relroot(path,root):
+    '''
+    remove the root folder path we supplied earlier
+    this is quick and dirty
+    '''
+    return  path.replace(root,'').strip(os.sep)
+
 
 if __name__== "__main__": main()
